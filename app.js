f79f1a4205eac4f1e2ce94b42a4ddee5d8d90712
tranminhkhang8198 <minhkhang811998@gmail.com>
2019-12-17 20:06:38 +0700

    Setup for express-handlebars

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 
 const app = express();
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+// Index route
+app.get('/', (req, res) => {
+    const title = 'Welcome';
+    res.render('index', {
+        title: title
+    });
+});
+
+// About route
+app.get('/about', (req, res) => {
+    res.render('about');
+});
 
 const port = 5000;
 
