e46782fe62b624610ed703caacd125300d8e60a0
tranminhkhang8198 <minhkhang811998@gmail.com>
2019-12-19 19:16:39 +0700

    Adding logout for user

--- routers/users.js
+++ routers/users.js
@@ -84,4 +84,11 @@ router.post('/register', (req, res) => {
     }
 });
 
+// Logout User
+router.get('/logout', (req, res) => {
+    req.logout();
+    req.flash('success_msg', 'You are logged out');
+    res.redirect('/users/login');
+});
+
 module.exports = router;
