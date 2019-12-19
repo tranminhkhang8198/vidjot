a37c5034abfc784fcdf47def16e0b34ac8abef7b
tranminhkhang8198 <minhkhang811998@gmail.com>
2019-12-19 19:33:30 +0700

    Adding access controll

--- routers/ideas.js
+++ routers/ideas.js
@@ -9,7 +9,7 @@ const Idea = mongoose.model('ideas');
 
 // Idea Index Page
 router.get('/', ensureAuthenticated, (req, res) => {
-    Idea.find({})
+    Idea.find({ user: req.user.id })
         .sort({ data: 'desc' })
         .then((ideas) => {
             res.render('ideas/index', {
@@ -43,7 +43,8 @@ router.post('/', ensureAuthenticated, (req, res) => {
     } else {
         const newUser = {
             title: req.body.title,
-            details: req.body.details
+            details: req.body.details,
+            user: req.user.id
         }
 
         new Idea(newUser)
@@ -62,9 +63,14 @@ router.get('/edit/:id', ensureAuthenticated, (req, res) => {
         _id: req.params.id
     })
         .then((idea) => {
-            res.render('ideas/edit', {
-                idea
-            });
+            if (idea.user != req.user.id) {
+                req.flash('error_msg', 'Not Authorized');
+                res.redirect('/ideas');
+            } else {
+                res.render('ideas/edit', {
+                    idea
+                });
+            }
         });
 });
 
